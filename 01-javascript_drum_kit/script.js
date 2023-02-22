dbaecb2b905998375ee83d50b4a57e8aa923cfc1
Bryan de Palma Martins Vida <bpmv96@outlook.com>
2023-02-22 12:22:49 -0300

    adiconado modo touch para o projeto drum

--- 01-javascript_drum_kit/script.js
+++ 01-javascript_drum_kit/script.js
@@ -7,14 +7,33 @@
   key.classList.add('playing');  
 };
 
+function playSoundTouch (event) {
+  const audio = document.querySelector(`audio[data-index="${event.target.innerText}"]`);
+  const key = document.querySelector(`.key[data-index="${event.target.innerText}"]`)
+  console.log(key)
+  if(!audio) return;
+  audio.currentTime = 0;
+  audio.play();
+  key.classList.add('playing');  
+};
+
 function removeTransition(event) {
   if(event.propertyName !== 'transform') return;
   this.classList.remove('playing');
 }
 
-window.addEventListener('keydown', playSound);
-
 const keys = document.querySelectorAll('.key');
+
+const arrayKey = [...keys];
+console.log(arrayKey)
+
+window.addEventListener('keydown' , playSound);
+keys.forEach((key) => {
+  key.addEventListener('touchstart', playSoundTouch);
+})
+
 keys.forEach((key) => {
   key.addEventListener('transitionend', removeTransition);
 })
+
+
