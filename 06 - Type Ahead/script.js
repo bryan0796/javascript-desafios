2c904dc06c77c376cfe3bd02fbd7f7d509d0c358
Bryan de Palma Martins Vida <bpmv96@outlook.com>
2023-02-28 23:32:30 -0300

    projeto de busca por populacao por estado

---
+++ 06 - Type Ahead/script.js
@@ -0,0 +1,55 @@
+class PopulacaoPorEstado {
+  constructor() {
+    this.estados = [];
+    this.populacao = [];
+    this.idEstado;
+    this.inputBusca = document.querySelector('.busca');
+    this.resultados = document.querySelector('.resultados');
+    this.fetchEstados = this.fetchEstados.bind(this);
+  }
+
+  buscarIdEstado() {
+    const regex = new RegExp(this.inputBusca.value, 'gi');
+    this.estados.map(estado => {
+      if (estado.nome.match(regex)) {
+        this.idEstado = estado.id;
+      }
+    });
+    this.buscarPopulacao(this.idEstado);
+  }
+
+  buscarPopulacao(id) {
+    fetch(`https://servicodados.ibge.gov.br/api/v1/projecoes/populacao/${id}`)
+      .then(response => response.json())
+      .then(json => {
+        this.populacao.push(json.projecao.populacao);
+        this.mostrarDados();
+      });
+  }
+  
+  mostrarDados() {
+    this.resultados.innerHTML = ` <li>População: ${this.populacao}</li>`;
+  }
+
+  fetchEstados() {
+    return fetch('https://servicodados.ibge.gov.br/api/v1/localidades/estados')
+    .then(response => response.json())
+    .then(json => {
+      this.estados.push(...json);
+      this.buscarIdEstado();
+    });
+  }
+  
+  evento() {
+    this.inputBusca.addEventListener('change', this.fetchEstados);
+    
+
+  }
+
+  init() {
+    this.evento();
+  } 
+}
+
+const iniciar = new PopulacaoPorEstado;
+iniciar.init();
